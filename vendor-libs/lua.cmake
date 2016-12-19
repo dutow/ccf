1aa04c3f973f52567ffff1e615334e50f8d59f58
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2016-12-19 10:42:31 +0100

    refactored binary extra dependency code

--- vendor-libs/lua.cmake
+++ vendor-libs/lua.cmake
@@ -1,12 +1,7 @@
-# Adds better-enums to the default libs / incldues
-
 INCLUDE_GUARD()
 INCLUDE( vendor-libs/vendor-helper )
 
 ADD_SUBDIRECTORY( "${CANCELLAR_BUILD_VENDOR_DIR}/lua" )
-
-APPEND_VENDOR_INCLUDES( "${CANCELLAR_BUILD_VENDOR_DIR}/lua/src" )
-APPEND_VENDOR_LIBRARIES( liblua )
-
-# HACK: include the directory where luaconf.h is stored
-APPEND_VENDOR_INCLUDES( "${CANCELLAR_BUILD_BINARY_DIR}/vendor/lua/" )
+# HACK! uses the build directory
+TARGET_INCLUDE_DIRECTORIES(  liblua INTERFACE "${CANCELLAR_BUILD_VENDOR_DIR}/lua/src" "${CANCELLAR_BUILD_BINARY_DIR}/vendor/lua/" )
+INTERNAL_PRESET_DEPENDENCY( liblua )
--- vendor-libs/sqlpp11-sqlite3.cmake
+++ vendor-libs/sqlpp11-sqlite3.cmake
@@ -4,6 +4,10 @@ INCLUDE( vendor-libs/vendor-helper )
 
 ADD_SUBDIRECTORY( "${CANCELLAR_BUILD_VENDOR_DIR}/sqlpp11-connector-sqlite3" )
 
-APPEND_VENDOR_INCLUDES( "${CANCELLAR_BUILD_VENDOR_DIR}/date" )
-APPEND_VENDOR_INCLUDES( "${CANCELLAR_BUILD_VENDOR_DIR}/sqlpp11-connector-sqlite3/include/" )
-APPEND_VENDOR_LIBRARIES( sqlpp11-connector-sqlite3 )
+# TODO: move to it's own file
+INTERNAL_DEFAULT_SINGLE_HEADER_ONLY_LIBRARY( "date" "date" )
+
+TARGET_INCLUDE_DIRECTORIES(  sqlpp11-connector-sqlite3 INTERFACE "${CANCELLAR_BUILD_VENDOR_DIR}/sqlpp11-connector-sqlite3/include/" )
+TARGET_LINK_LIBRARIES( sqlpp11-connector-sqlite3 date )
+
+INTERNAL_PRESET_DEPENDENCY( sqlpp11-connector-sqlite3 )
