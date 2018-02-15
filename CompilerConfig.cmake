875cf47654a618aa3848984883f1ae236d7f7428
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2018-02-15 17:36:09 +0100

    Fixed issues with clang-cl and ninja on windowd

--- CompilerConfig.cmake
+++ CompilerConfig.cmake
@@ -5,34 +5,25 @@ IF( MSVC )
   IF( CMAKE_VS_PLATFORM_TOOLSET MATCHES "LLVM-vs2014" )
     MESSAGE(NOTICE "Using clang-cl with tooclhain")
     SET (CLANG_CL ON)
-	SET (CLANG_CL_TOOLCHAIN ON)
+	  SET (CLANG_CL_TOOLCHAIN ON)
   ELSEIF( CMAKE_CXX_COMPILER MATCHES ".*clang-cl.*" )
     MESSAGE(NOTICE "Using clang-cl without tooclhain")
-	SET (CLANG_CL ON)
-	SET (CLANG_CL_TOOLCHAIN OFF)
+	  SET (CLANG_CL ON)
+	  SET (CLANG_CL_TOOLCHAIN OFF)
   ELSE()
     MESSAGE(NOTICE "Using MSVC cl")
     SET (CLANG_CL OFF)
-	SET (CLANG_CL_TOOLCHAIN OFF)
+  	SET (CLANG_CL_TOOLCHAIN OFF)
   ENDIF()
   IF( CLANG_CL AND NOT CLANG_CL_TOOLCHAIN )
-    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fmsc-version=1900") # compatible with VS2015
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fmsc-version=1910") # compatible with VS2017
     IF( CANCELLAR_ARCH_64 )
       SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m64")
     ELSEIF( CANCELLAR_ARCH_32 )
-        SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
+      SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
     ELSE()
-	  MESSAGE(FATAL_ERROR "Unknown architechture for clang-cl")
+	    MESSAGE(FATAL_ERROR "Unknown architechture for clang-cl")
     ENDIF()
-  ENDIF()
-  IF( CLANG_CL )
-	  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Xclang -Wno-c++1z-extensions") # allow experimental C++17
-	  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unknown-pragmas") # msvc pragmas in boost
-	  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-microsoft-unqualified-friend") # msvc specific code in boost
-	  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-microsoft-enum-value") # msvc specific code in boost
-    IF( CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 3.9 )
-  	  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-expansion-to-defined") # TODO: too much easylogging warnings with clang4
-    ENDIF ()
   ENDIF()
 ELSE()
   # The C++ standard library has to be set globally to ensure that dependencies are built with the same
