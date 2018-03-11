ff916f75f97a69bcbb607926d9f8402919c6e61a
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2018-03-11 20:41:44 +0100

    Added backward-cpp

---
+++ vendor-libs/backward-cpp.cmake
@@ -0,0 +1,14 @@
+# Adds the standalone ASIO as an interface library
+
+INCLUDE_GUARD()
+INCLUDE( vendor-libs/vendor-helper )
+
+
+INTERNAL_HEADER_ONLY_LIBRARY( "backward-cpp" "backward-cpp" )
+
+IF ( UNIX )
+  FIND_PACKAGE( Dwarf REQUIRED )
+  FIND_PACKAGE( LibElf REQUIRED )
+  TARGET_LINK_LIBRARIES( "backward-cpp" INTERFACE ${LIBDWARF_LIBRARIES} ${LIBELF_LIBRARIES})
+  TARGET_INCLUDE_DIRECTORIES( "backward-cpp" INTERFACE ${LIBDWARF_INCLUDE_DIRS} ${LIBELF_INCLUDE_DIRS})
+ENDIF ()
