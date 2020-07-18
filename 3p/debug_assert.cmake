06849c8b0b02de1d94d7867409ce2e605fa400ba
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2020-07-18 08:59:42 +0200

    3p update and use newer debug assert

--- 3p/debug_assert.cmake
+++ 3p/debug_assert.cmake
@@ -1,7 +1,7 @@
 set(_CCF_DEP_REPOSITORY "https://github.com/foonathan/debug_assert.git")
 set(_CCF_DEP_NAME "debug_assert")
-set(_CCF_DEP_DEFAULT_TYPE "TAG")
-set(_CCF_DEP_DEFAULT_VER "v1.3.3")
+set(_CCF_DEP_DEFAULT_TYPE "BRANCH")
+set(_CCF_DEP_DEFAULT_VER "master")
 #set(_CCF_DEP_IGNORE_CHANGES ON)
 
 macro(_ccf_3p_target_callback)
