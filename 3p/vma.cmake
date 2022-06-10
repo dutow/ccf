6f590b9182e9a776b79de3ff4fb27f473f4eb2be
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2022-06-10 18:49:50 +0200

    adding vma

---
+++ 3p/vma.cmake
@@ -0,0 +1,9 @@
+set(_CCF_DEP_REPOSITORY "https://github.com/malte-v/VulkanMemoryAllocator-Hpp.git")
+set(_CCF_DEP_NAME "vma")
+set(_CCF_DEP_DEFAULT_TYPE "BRANCH")
+set(_CCF_DEP_DEFAULT_VER "master")
+#set(_CCF_DEP_IGNORE_CHANGES ON)
+
+macro(_ccf_3p_target_callback)
+  ccf_3p_lib_header_only( "vma" "." )
+endmacro()
